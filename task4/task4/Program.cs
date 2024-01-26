dd515861f9be8ea2f88ff19f74bf0051994745a5
EIderlyDreamer <viktor-dranishnikov-vd@mail.ru>
2024-01-26 14:16:50 +0300

    Добавьте файлы проекта.

---
+++ task4/task4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите сумму вклада: ");
+            double depositAmount = Convert.ToDouble(Console.ReadLine());
+            double interestRate;
+
+            if (depositAmount < 100)
+            {
+                interestRate = 0.05;
+            }
+            else if (depositAmount >= 100 && depositAmount <= 200)
+            {
+                interestRate = 0.07;
+            }
+            else
+            {
+                interestRate = 0.1;
+            }
+
+            double totalAmount = depositAmount + (depositAmount * interestRate);
+            Console.WriteLine($"Сумма вклада с начисленными процентами: {totalAmount}");
+
+            Console.ReadKey();
+        }
+    }
+}
